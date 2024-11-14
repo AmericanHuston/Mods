c2aaf4baa68f81efabe6f19fa9f02925ad314264
American Huston <Americanhuston@gmail.com>
2024-11-14 15:31:28 -0700

    Fixed userdata errors, added on the event that a player kills another player

--- enhanced_clans/clans/kiwi_sea_test.lua
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -1,12 +1,26 @@
 local storage = core.get_mod_storage()
 
 core.register_on_dieplayer(function(player, reason)
-    --clansmod.killer(player, player)
-    core.chat_send_player(player, reason)
+    local player = player:get_player_name()
+    local level = storage:get_int(player .. "-level")
+    for i,v in pairs(reason) do
+        if type(v) == "userdata" then
+            core.chat_send_player(player, v:get_player_name() .. " has killed you")
+            clansmod.killer(player, v:get_player_name())
+        end
+    end
+    if level ~= 0 then
+        level = level - 1
+        storage:set_int(player .. "-level", level)
+        core.chat_send_player(player, "Your level is now " .. level)
+    else
+        core.chat_send_player(player, "Your level is " .. level)
+    end
 end
 )
 
 core.register_on_joinplayer(function(player, last_login)
+    local player = player:get_player_name()
     if last_login == nil then
         storage:set_int(player .. "-level", 0)
     end
@@ -14,16 +28,20 @@ end
 )
 
 function clansmod.killer(killer, victim)
-    local klevel = storage:get_int(killer .. "-level")
-    local vlevel = storage:get_int(victim .. "-level")
+    local killercs = killer .. "-level"
+    local victimcs = victim .. "-level"
+    local klevel = storage:get_int(killercs)
+    local vlevel = storage:get_int(victimcs)
 
     if vlevel ~= 0 then
         vlevel = vlevel - 1
-        storage:set_int(victim .. "-level", vlevel)
+        storage:set_int(victimcs, vlevel)
     end
 
     if klevel ~= 5 then
         klevel = klevel + 1
-        storage:set_int(killer .. "-level", klevel)
+        storage:set_int(killercs, klevel)
     end
+
+    core.chat_send_player(victim, "Your level is now " .. storage:get_int(victimcs))
 end
