adfd396aa4db3bcfa415edc438983b7525f08905
kiwiSea <bensederstrom@gmail.com>
2024-11-16 04:52:43 +0000

    working on avergeing out players lvls in clans.

---
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -0,0 +1,11 @@
+clansmod.add_to_clan(issuer, playername, false, tbd)
+
+core.register_chatcommand("assign_clan", {
+    func = function(name, param)
+        local average_team_lvl
+        local player = core.get_player_by_name(name)
+        local player = player:get_player_name()
+        if
+})
+
+function clansmod.players_per_team(clan)
