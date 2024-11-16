40277b97a8d0d4ab1e196c9daf636ddec8e51770
American Huston <Americanhuston@gmail.com>
2024-11-16 14:07:50 -0700

    Working on clansmod.sorting_alg

--- enhanced_clans/clans/kiwi_sea_test.lua
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -34,6 +34,19 @@ function clansmod.players_online()
     return online
     
 end
+
+function clansmod.players_in_clan(clan)
+    local playercs
+    local tbl_players_in_clan
+    for i,v in ipairs(core.get_connected_players) do
+        playercs = v:get_player_name() .. "-clan"
+        if storage:get_string(playercs) == clan then
+            table.insert(tbl_players_in_clan, v:get_player_name())
+        end
+    end
+    return tbl_players_in_clan
+end
+
 -- clansmod.sorting_algorithim. the average is going to be determined by the median, not the mean or mode.
 function clansmod.sorting_alg(average, median, levels)
     local online = clansmod.players_online()
@@ -41,12 +54,24 @@ function clansmod.sorting_alg(average, median, levels)
     local clan_lvl
     local ptable
 
+    for i,v in ipairs(ptable) do
+        player_lvl_cs = v .. "-level"
+        local player_lvl = storage:get_string(player_lvl_cs)
+        if clan_lvl == nil then
+            clan_lvl = player_lvl
+        else
+            clan_lvl = clan_lvl + player_lvl
+        end
+    end
+
     for i,v in ipairs(clansmod.clans) do
+        local clan = v
+        for _, a in ipairs(clansmod.players_in_clan(clan)) do
+            
         table.insert(clan_members, clansmod.players_per_clan(v))
     end
 
     for i,v in ipairs(core.get_connected_players()) do
         table.insert(ptable, player:get_player_name())
     end
-    
-    for i,v in ipairs(ptable) do
+end
