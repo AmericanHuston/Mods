f7a0d374b5298cb5f741e28d4efdcd593c72c7cc
American Huston <Americanhuston@gmail.com>
2024-11-16 14:53:17 -0700

    AAAA

--- enhanced_clans/clans/kiwi_sea_test.lua
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -47,41 +47,33 @@ function clansmod.players_in_clan(clan)
     return tbl_players_in_clan
 end
 
--- clansmod.sorting_algorithim. the average is going to be determined by the median, not the mean or mode.
-function clansmod.sorting_alg(average, median, levels)
-    local online = clansmod.players_online()
-    local clan_members
-    local clans_lvl --2d table
-    local ptable
-
-    for i,v in ipairs(ptable) do
-        local player_lvl_cs = v .. "-level"
-        local player_lvl = storage:get_string(player_lvl_cs)
-        if clans_lvl == nil then
-            clans_lvl = player_lvl
-        else
-            clans_lvl = clans_lvl + player_lvl
-        end
-    end
-
+function clansmod.clan_level(clan)
     for i,v in ipairs(clansmod.clans) do
         local clan = v
         local clan_players = clansmod.players_in_clan(clan)
         for _, a in ipairs(clan_players) do
-            local playername = a:get_player_name()
-            local playercs = playername .. "-clan"
-            local player_lvl_cs = playername .. "-level"
-            local player_clan = storage:get_string(playercs)
-            local player_lvl = storage:get_string(player_lvl_cs)
-            if clans_lvl == nil then
-                clans_lvl = player_lvl
+            local player_lvl_cs = a .. "-level"
+            local player_lvl = storage:get_int(player_lvl_cs)
+            if clan_lvl == nil then
+                clan_lvl = player_lvl
             else
-                clans_lvl = clans_lvl + player_lvl
+                clan_lvl = clan_lvl + player_lvl
             end
-        table.insert(clan_members, clansmod.players_per_clan(v))
+        end
     end
+    return clan_lvl
+end
 
-    for i,v in ipairs(core.get_connected_players()) do
-        table.insert(ptable, player:get_player_name())
+-- clansmod.sorting_algorithim. the average is going to be determined by the median, not the mean or mode.
+function clansmod.sorting_alg()
+    local online = clansmod.players_online()
+    local clan_lvl
+    local clan_players
+    
+    for i,v in ipairs(clansmod.clans) do
+        local clan = v
+        clan_players = clansmod.players_in_clan(clan)
+        clan_lvl = clansmod.clan_level(clan)
+        storage:set_int(clan .. "-level", clan_lvl)
     end
 end
