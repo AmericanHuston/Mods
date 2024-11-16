d3395f79b19efcfd90203a8104e56a85ed62775e
kiwiSea <bensederstrom@gmail.com>
2024-11-16 05:14:14 +0000

    Added local storage to players_online.

--- enhanced_clans/clans/kiwi_sea_test.lua
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -1,19 +1,26 @@
---clansmod.add_to_clan(issuer, playername, false, tbd)
+local storage = core.get_mod_storage()
+clansmod.add_to_clan(issuer, playername, false, tbd)
 
--- core.register_chatcommand("assign_clan", {
---     func = function(name, param)
---         local average_team_lvl
---         local player = core.get_player_by_name(name)
---         local player = player:get_player_name()
---         if
--- })
+core.register_chatcommand("assign_clan", {
+    func = function(name, param)
+       local average_team_lvl
+        local player = core.get_player_by_name(name)
+        local player = player:get_player_name()
+     if
+})
 
-function clansmod.players_per_team(clan)
+function clansmod.players_per_clan(clan)
+    
+end
+
+
+function clansmod.players_online()
     local ptable
     for _, player in ipairs(minetest.get_connected_players()) do
         table.insert(ptable, player:get_player_name())
     end
 
     local online = #ptable
+    return online
+    
 end
---hi
