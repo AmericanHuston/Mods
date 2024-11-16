c6dc4e07841290cf8b27f539db317262ac12fe3e
American Huston <Americanhuston@gmail.com>
2024-11-15 21:52:46 -0700

    Adding player sorting, and adding clans

--- enhanced_clans/clans/clans.lua
+++ enhanced_clans/clans/clans.lua
@@ -2,11 +2,22 @@ local storage = core.get_mod_storage()
 clansmod = {}
 clansmod.clans = {"mesa", "mountain", "river", "grassland"}
 
+core.register_on_prejoinplayer()
+
 core.register_privilege("eventadmin", {
     description = "Can issue all clan commands",
     give_to_singleplayer = true
 })
 
+function clansmod.add_clan(issuer, clanname)
+    local found
+    for i,v in pairs(clansmod.clans) do
+        if v == clanname then
+            found = true
+        else
+            found = false
+    end
+    
 
 function clansmod.add_to_clan(issuer, playername, random, clan) --MUST BE PLAYER NAME, NOT USERDATA
     local playercs = playername .. "-clan"
