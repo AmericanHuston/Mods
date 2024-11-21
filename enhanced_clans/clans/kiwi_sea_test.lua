be7d6bcc99b7339f2c3cc1b7004fbc804f25f10c
American Huston <Americanhuston@gmail.com>
2024-11-21 16:07:57 -0700

    Worked on clansmod.sorting_alg, checking for if adding a player to a clan would break the equality

--- enhanced_clans/clans/kiwi_sea_test.lua
+++ enhanced_clans/clans/kiwi_sea_test.lua
@@ -25,43 +25,64 @@ function clansmod.players_online()
     return online
 end
 
--- function clansmod.players_in_clan(clan)
---     local playercs
---     local tbl_players_in_clan = {}
---     for i,v in ipairs(core.get_connected_players) do
---         playercs = v:get_player_name() .. "-clan"
---         if storage:get_string(playercs) == clan then
---             table.insert(tbl_players_in_clan, v:get_player_name())
---         end
---     end
---     return tbl_players_in_clan
--- end
+function clansmod.players_in_clan(clan)
+    local playercs
+    local tbl_players_in_clan = {}
+    for i,v in ipairs(core.get_connected_players) do
+        playercs = v:get_player_name() .. "-clan"
+        if storage:get_string(playercs) == clan then
+            table.insert(tbl_players_in_clan, v:get_player_name())
+        end
+    end
+    return tbl_players_in_clan
+end
 
-function clansmod.clan_level()
+function clansmod.clan_level(currentClan)
     local clan_lvl
-    for i,v in ipairs(clansmod.clans) do
-        local clan = v
-        local clan_players = clansmod.players_in_clan(clan)
-        for _, a in ipairs(clan_players) do
-            local player_lvl_cs = a .. "-level"
-            local player_lvl = storage:get_int(player_lvl_cs)
+    if clansmod.clan_exists(currentClan) then
+        local clan_players = clansmod.players_in_clan(currentClan)
+        for i, v in ipairs(clan_players) do
+            local playerls = v .. "-level" --Player Level Storage
+            local player_lvl = storage:get_int(playerls)
             if clan_lvl == nil then
                 clan_lvl = player_lvl
             else
                 clan_lvl = clan_lvl + player_lvl
             end
+            clan_lvl = clan_lvl / #clan_players
         end
+        return clan_lvl
+    else
+        core.log("error", "Clan " .. currentClan .. " not found.")
     end
-    return clan_lvl
+end
+
+function clansmod.get_player_level(playername)
+    local playercs = playername .. "-clan"
+    local player_lvl = storage:get_string(playercs)
+    return player_lvl
 end
 
 -- clansmod.sorting_algorithim. the average is going to be determined by the median, not the mean or mode.
-function clansmod.sorting_alg()
-    local online = clansmod.players_online()
-    local clan_lvl
+function clansmod.sorting_alg(playername) --Must not be userdata
+    local all_clan_average = 0
+    local assignable_clans = {}
+    local playerls = playername .. "-level"
     for i,v in ipairs(clansmod.clans) do
         local clan = v
-        clan_lvl = clansmod.clan_level(clan)
-        storage:set_int(clan .. "-level", clan_lvl)
+        local clan_lvl = clansmod.clan_level(clan)
+        local clanls = clan .. "-level"
+        storage:set_int(clanls, clan_lvl)
+        all_clan_average = all_clan_average + clan_lvl
+    end
+    all_clan_average = all_clan_average/ #clansmod.clans
+    for _,a in ipairs(clansmod.clans) do
+        local clan = a
+        local clanls = clan .. "-level"
+        local clan_lvl = storage:get_int(clanls)
+        local players_in_clan = #clansmod.players_in_clan(clan)
+        if (storage:get_int(playerls) + clan_lvl)/players_in_clan < all_clan_average then
+            table.insert(assignable_clans, clan)
+        end
     end
 end
