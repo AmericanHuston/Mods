1426cc9b435928145eb2974fd3d1296aa7a91e17
American Huston <Americanhuston@gmail.com>
2024-11-27 16:51:02 -0700

    Testing conflicts with gitea

---
+++ enhanced_clans/clan_food/init.lua
@@ -0,0 +1,4 @@
+core.register_on_chat_message(function(sender, message)
+    --Need to find a way to get "@" and then the player name
+    --likely just need to do param match... "@" ... untill whitespace
+)
