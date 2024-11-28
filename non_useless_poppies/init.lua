d9f42e3ab3d4e40220b2e53d3b8bf30f7ea055a9
American Huston <Americanhuston@gmail.com>
2024-11-27 23:00:59 -0700

    Balenced out the stuff

---
+++ non_useless_poppies/init.lua
@@ -0,0 +1,9 @@
+core.register_craft({
+    output = "mcl_core:iron_ingot",
+    recipe = {
+        "", "", "",
+        "", "mcl_flowers:poppy", "",
+        "", "", ""
+    }
+})
+--Nah that's all
