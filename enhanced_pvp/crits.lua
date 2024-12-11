a605bfe0c0c1ba281630e0d92d13037472ccce1b
American Huston <Americanhuston@gmail.com>
2024-12-10 23:32:34 -0700

    Almost done with crits AAAAA

--- enhanced_pvp/crits.lua
+++ enhanced_pvp/crits.lua
@@ -1,27 +1,36 @@
-function enhanced_pvp.crit(punchername, vel)
-    local caps = {full_punch_interval = 0.9,
-    max_drop_level = 0,
-    groupcaps = {
-        crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
-        snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
-        oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
-        },
-    damage_groups = {fleshy=3},
-    }
-    if vel[1] < 0 then
-        local inv = punchername:get_inventory()
-        local stack = inv:get_stack("hand", 1)
-        stack:set_tool_capabilities(caps)
-        inv:set_stack("hand", 1, stack)
-        punchername:punch(punchername, nil, caps, nil)
+core.register_globalstep(function()
+    for _,player in ipairs(core.get_connected_players()) do
+        local vel = player:get_velocity().y
+        if vel < 0 then
+            core.override_item("", {
+                tool_capabilities = {
+                    full_punch_interval = 0.9,
+                    max_drop_level = 0,
+                    groupcaps = {
+                        crumbly = {times={3.00, 0.70, 0.20}, uses=0, maxlevel=1},
+                        cracky  = {times={2.00, 0.70, 0.30}, uses=0, maxlevel=1},
+                        snappy  = {times={0.40, 0.30, 0.20}, uses=0, maxlevel=1},
+                        choppy  = {times={3.00, 2.00, 1.00}, uses=0, maxlevel=1},
+                        oddly_breakable_by_hand = {times={3.50, 2.00, 0.70}, uses=0},
+                        -- dig_immediate group doesn't use value 1. Value 3 is instant dig
+                    },
+                    damage_groups = {fleshy = 10},
+                }
+            })
+        else
+            core.override_item("", {
+                wield_scale = {x=1,y=1,z=2.5},
+                tool_capabilities = {
+                    full_punch_interval = 0.9,
+                    max_drop_level = 0,
+                    groupcaps = {
+                        crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+                        snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+                        oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}
+                    },
+                    damage_groups = {fleshy=1},
+                }
+            })
+        end
     end
-end
-
-core.register_on_joinplayer(function(name, last_login)
-    local player = name:get_player_name()
-    local vel = player:get_velocity()
-    while core.get_player_by_name() ~= nil do
-        enhanced_pvp.crits(player, vel)
-    end
-end
-)
+end)
