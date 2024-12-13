fdc0be1bd9632bc4c7f874df21ad5babccf8a55d
American Huston <Americanhuston@gmail.com>
2024-12-13 13:47:13 -0700

    Fixing crits damage calculation

--- enhanced_pvp/crits.lua
+++ enhanced_pvp/crits.lua
@@ -3,7 +3,7 @@ core.register_on_punchplayer(function(player_got_hit, hitter, time_from_last_pun
     local vel = hitter:get_velocity().y
         if vel < 0 then
             local standard_damage = tool_capabilities.damage_groups.fleshy
-            local new_damage = standard_damage + math.round(-vel/3)
+            local new_damage = standard_damage + -1
             if standard_damage < new_damage then
                 local health = player_got_hit:get_hp()
                 player_got_hit:set_hp(health - -(standard_damage - new_damage), "CRITICALLL")
