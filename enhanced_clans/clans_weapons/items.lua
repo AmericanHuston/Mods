fad13dc34a807fff1ecbf27c68aa4db231b7d1f7
American Huston <Americanhuston@gmail.com>
2024-12-16 14:25:45 -0700

    Adding some textures

--- enhanced_clans/clans_weapons/items.lua
+++ enhanced_clans/clans_weapons/items.lua
@@ -1,6 +1,14 @@
-minetest.register_craftitem("clans_weapons:weapon1", {
+core.register_craftitem("clans_weapons:weapon1", {
     description = "A Powerful Weapon",
-    inventory_image = "clans_weapons_water_sword.png"
+    inventory_image = "clans_weapons_water_sword.png",
+    tool_capabilities = {
+        full_punch_interval = 1.0,
+        max_drop_level = 1,
+        groupcaps = {
+            snappy = {times = {[1] = 1.90, [2] = 0.90, [3] = 0.30}, uses = 20, maxlevel = 3},
+        },
+        damage_groups = {fleshy = 15},
+    }
 })
 
 core.register_craft({
